2643e4a52cf65298952efc2408650e519bb248f6
Maxime <MaxBen@MacBook-Pro-de-Beneteau.local>
2016-03-11 10:16:57 +0100

    new css

--- drdjscs/html/js/controllers.js
+++ drdjscs/html/js/controllers.js
@@ -22,20 +22,22 @@ app.controller('IntroCtrl', ['$scope', '$http', '$timeout', function ($scope, $h
         $scope.pieData = data;
         $scope.pieDynamicData = data[0];
     });
-    
+    var colors = ["#00AF9B","#3C78F"];
     $scope.options = {
             chart: {
                 type: 'multiChart',
-                height: 450,
+                height: 380,
+                width:900,
                 margin : {
                     top: 30,
                     right: 60,
                     bottom: 50,
                     left: 70
                 },
-                color: d3.scale.category10().range(),
+                color: d3.scale.category10().range(colors,colors[1]),
+
                 useInteractiveGuideline: true,
-                duration: 1000,
+                duration: 4000,
                 useInteractiveGuideline: true,
                 dispatch: {
                     stateChange: function(e){ console.log("stateChange"); },
@@ -44,32 +46,25 @@ app.controller('IntroCtrl', ['$scope', '$http', '$timeout', function ($scope, $h
                     tooltipHide: function(e){ console.log("tooltipHide"); }
                 },
                 xAxis: {
-                    axisLabel: 'Période de 1997 - 2015',
+                    //axisLabel: 'Période de 1997 - 2015',
                     tickFormat: function(d){
                         return d3.format('f')(d);
                     }
                 },
                 yAxis1: {
-                    axisLabel: 'Création d\'emplois',
+                    //axisLabel: 'Création d\'emplois',
                     tickFormat: function(d){
                         return d3.format(',.1f')(d);
                     }
                 },
                 yAxis2: {
-                    axisLabel: 'Chômage en pourcentage',
+                    //axisLabel: 'Chômage en pourcentage',
                     tickFormat: function(d){
                         return d3.format(',.1f')(d);
                     }
                 }
-            },
-            title: {
-                enable: true,
-                html: '<h2>Comparaison entre création d\'emplois et chômage</h2>'
-            },
-            subtitle: {
-                enable: true,
-                html: '<p style="color: red;">Données sur le pays de la loire Atlantique</p>'
-            }/*,
+            }
+           /*,
             css: {
                     'text-align': 'center',
                     'margin': '10px 13px 0px 7px'
