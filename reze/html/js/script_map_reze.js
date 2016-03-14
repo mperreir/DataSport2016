2f615ed8b3307ec73372bdf5df714b79838a94cd
Junlang <june_lang@outlook.com>
2016-03-14 21:59:42 +0100

    change size of map and font size

--- reze/html/js/script_map_reze.js
+++ reze/html/js/script_map_reze.js
@@ -1,4 +1,5 @@
 $("#container2").height($("#container1").height());
+var selected = false;
         var svg = d3.select("#svg").attr("preserveAspectRatio","none");
         svg1 = svg.selectAll("polygon").call(d3.behavior.zoom().scaleExtent([1, 2]).on("zoom", zoom)).on("click",clickHandler);
         var i = 1;
@@ -12,11 +13,18 @@ $("#container2").height($("#container1").height());
         function mouseOverHandler(e){
             //var loc = cursorPoint(e);
             //console.log(loc);
-            $("#infos").css("opacity",0);
-            $("#infos").css("visibility","visible");
-            var fill = $(this).parent().attr("fill");
-            $("#infos").css("background", fill);
-            $("#infos").animate({opacity: 1},1000);
+            if(selected){
+                return;
+            }
+            else{
+                $("#infos").stop();
+                $("#infos").css("opacity",0);
+                $("#infos").css("visibility","visible");
+                var fill = $(this).parent().attr("fill");
+                $("#infos").css("background", fill);
+                $("#infos").animate({opacity: 1},1000);
+            }
+            
             //afficher les donnees....
         };
         
@@ -24,9 +32,14 @@ $("#container2").height($("#container1").height());
             
             //d3.select("#"+this.id).attr("transform","translate(0,0)scale(1)");
             //console.log(this);
-            $("#infos").css("opacity",0);
-            $("#infos").stop();
-            $("#infos").css("visibility","hidden");
+            if(selected){
+                return;
+            }
+            else{
+                $("#infos").stop();
+                $("#infos").css("opacity",0);               
+                $("#infos").css("visibility","hidden");
+            }
         };
         var currentCircle;
        function clickHandler(){
@@ -38,15 +51,24 @@ $("#container2").height($("#container1").height());
                    console.log(currentCircle);
                    $(currentCircle).attr("r",7.8);
                }
-                  
+               //changing infos
+               $("#infos").stop();
+               $("#infos").css("opacity",0);               
+               $("#infos").css("visibility","hidden");
                $("#infos").css("visibility","visible");
                $("#infos").css("background", fill);
+               $("#infos").animate({opacity: 1},1000);
+               
                d3.selectAll("circle").attr("fill","#c0c0c0");
                d3.select("#" + this.id).attr("fill", fill).attr("r",10);
                currentCircle = this;
+               //$(this).attr("selected",true);
+               selected = true;
            }
            else{
                $("#infos").css("visibility","hidden");
                d3.selectAll("circle").attr("fill",$(this).parent().attr("fill")).attr("r",7.8);
+               //d3.selectAll("circle").attr("selected",false);
+               selected = false;
            }
        };
