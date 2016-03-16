eb24055ea679c8bcde61b733794dbf2c1f18eba9
Pauwel <pauwel.d-almeida-ayivi@etu.univ-nantes.fr>
2016-03-16 01:01:04 +0100

    MAJ Pauwel

--- drdjscs/html/js/script_carte.js
+++ drdjscs/html/js/script_carte.js
@@ -91,8 +91,30 @@ map.on('click', function (e) {
     });
 });
 
+addLayer('Contours', 'contours');
+addLayer('Markers', 'markers');
 
+function addLayer(name, id) {
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = name;
 
-function perreines(){
+    link.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
 
+        var visibility = map.getLayoutProperty(id, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(id, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(id, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
 }
