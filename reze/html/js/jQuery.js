fa8526b7577c134ff71195b100995a9fc326f4e2
Junlang <june_lang@outlook.com>
2016-03-16 11:06:29 +0100

    add animation that count number

--- reze/html/js/jQuery.js
+++ reze/html/js/jQuery.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
 });
+
+/*$('#fullpage').afterLoad = function(al,index){
+    if(al = "4thPage"){
+        $('.timer').countTo({
+            from: 0,
+            to: 2500,
+            speed: 3000,
+            refreshInterval: 50
+        });
+    }
+};*/
