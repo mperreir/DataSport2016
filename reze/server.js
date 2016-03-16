fec3c8ef81576bd70741307bec0148a574f93628
e14a688j <fatima-zahra.magouri@etu.univ-nantes.fr>
2016-03-16 20:39:16 +0100

    carte 2 quasi finie

--- reze/server.js
+++ reze/server.js
@@ -1,8 +1,12 @@
+// load express module for creating server
 var express = require('express');
-var path = require('path');
+// create your server app
 var app = express();
-
-// serve static content from the html directory
-app.use(express.static(path.join(__dirname, 'html')));
-
-module.exports = app;
+// specifing static files
+app.use(express.static('html'));
+// creating your server
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
