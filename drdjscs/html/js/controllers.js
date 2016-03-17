e8292f65dd10bafe1e80046380bea61534157012
Maxime <MaxBen@MacBook-Pro-de-Beneteau.local>
2016-03-17 14:13:29 +0100

    dark

--- drdjscs/html/js/controllers.js
+++ drdjscs/html/js/controllers.js
@@ -108,7 +108,7 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
               x: function(d){ return d.x; },
               y: function(d){ return d.y; },
               transitionDuration: 100,
-              color: ['rgba(0, 175, 155, 1)','rgba(182, 174, 195, 1)'],
+              color: ['rgba(0, 175, 155, 0.8)','rgba(182, 174, 195, 0.8)'],
               xAxis: {
 
               },
@@ -117,9 +117,13 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
                   tickFormat: function(d){
                       return d3.format('.02f')(d);
                   },
-                  axisLabelDistance: 300
+                  axisLabelDistance: 300,
+                  css: {
+                opacity:"0.4",
+            }
               },
               yAxis2: {
+
                   
               },
               useInteractiveGuideline: true,
@@ -141,7 +145,11 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
                 },
                 callback: function(){
                   d3.selectAll('.nvd3.nv-legend g').style('fill', "RGBA(55, 196, 180, 1)")
-            }}
+            },
+            css: {
+                opacity:"0.4",
+            }
+        }
         };
     $scope.optionsMini = {
             chart: {
@@ -156,7 +164,7 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
               x: function(d){ return d.x; },
               y: function(d){ return d.y; },
               transitionDuration: 100,
-              color: ['rgba(0, 175, 155, 1)','rgba(182, 174, 195, 1)'],
+              color: ['rgba(0, 175, 155, 0.2)','rgba(182, 174, 195, 0.2)'],
               xAxis: {
 
               },
@@ -203,7 +211,7 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
     function updateColor(debut, fin) {
 
         for (var i = 0; i < 19; i++) {
-            d3.selectAll("rect.nv-bar")[0][i].style= "fill: RGBA(230, 230, 230, 1)";
+            d3.selectAll("rect.nv-bar")[0][i].style= "fill: rgba(99, 120, 135, 0.2)";
         }
         for (var i = debut; i < fin; i++) {
             d3.selectAll("rect.nv-bar")[0][i].style= "fill: RGBA(55, 196, 180, 1)";
@@ -355,7 +363,7 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
         $scope.barAnnee = "1997 - 2015";
         emplois(0, 18);
         for (var i = 0; i < 19; i++) {
-            d3.selectAll("rect.nv-bar")[0][i].style= "fill: rgba(0, 175, 155, 1)"
+            d3.selectAll("rect.nv-bar")[0][i].style= "fill: rgba(99, 120, 135, 0.3)"
         }
         $scope.courbeDescription = "La stratégie régionale en matière d’emploi fera l’objet d’un examen spécifique au sein de la commission territoriale. Un renforcement de ce moyen d’intervention devra être mis en œuvre sur la période 2013-2016 dans chaque région."
         $scope.courbeDescriptionTitre = "Qu\'est-ce que c\'est?"
@@ -519,7 +527,7 @@ appHyblab.controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scop
             clipEdge: false,
             duration: 40,
             stacked: false,
-            color: ['RGBA(230, 230, 230, 1)'],
+            color: ['RGBA(55, 196, 180, 1)'],
             xAxis: {
                 axisLabel: '',
                 showMaxMin: false,
