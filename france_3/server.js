d2ac59816db0b9a336fc612f557a61957e6dfd93
Matthieu Perreira Da Silva <matthieu.perreiradasilva@univ-nantes.fr>
2016-03-22 11:24:56 +0100

    Fixes France3 project + additional cleanup

--- france_3/server.js
+++ france_3/server.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use('/', express.static('./build/'));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
-var server = app.listen(8080, function () {
+/*var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('http://%s:%s', host, port);
-});
+});*/
 
 module.exports = app;
